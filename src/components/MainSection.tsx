d3357ed7d0935bf8a8f11475e8dc054ac44111a7
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-07-15 14:39:09 +0100

    created html and css

---
+++ src/components/MainSection.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MainSection() {
+  return (
+    <main className="wrapper">
+      <section className="create-post-section">
+        {/* <!-- Go to create-post-section.html for the template --> */}
+      </section>
+      <section className="feed">
+        {/* <!-- Go to feed.html for the template --> */}
+      </section>
+    </main>
+  );
+}
+
+export default MainSection
---
+++ src/components/Root.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Root() {
+  return (
+    <div id="root">
+      <header className="main-header">
+        {/* <!-- Go to 2-header-section.html --> */}
+      </header>
+      <main className="wrapper">
+        {/* <!-- Go to 3-header-section.html --> */}
+      </main>
+    </div>
+  );
+}
+
+export default Root
